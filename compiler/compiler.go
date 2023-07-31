e7e81a35bce7dae94d27ff933ae81c8c1e9aa751
Chris Hsu <chrisfishbob@gmail.com>
2023-07-30 20:13:28 -0700

    The compiler now compiling constants with constant pool

---
+++ compiler/compiler.go
@@ -0,0 +1,81 @@
+package compiler
+
+import (
+	"seville/ast"
+	"seville/object"
+	"seville/opcode"
+)
+
+type Compiler struct {
+	instructions opcode.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: opcode.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(opcode.OpConstant, c.addConstant(integer))
+	}
+
+	return nil
+}
+
+func (c *Compiler) emit(op opcode.Opcode, operands ...int) int {
+	ins := opcode.Make(op, operands...)
+	pos := c.addInstruction(ins)
+	// starting position of the just-emitted instruction
+	return pos
+}
+
+func (c *Compiler) addInstruction(ins []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions opcode.Instructions
+	Constants    []object.Object
+}
